7c8a2a047955fc772deacd8a97cfc6b28b0b419a
Sean Murphy <sean@iamseanmurphy.com>
2024-09-24 10:59:16 +0000

    1.5

---
+++ gateway-aqayepardakht-for-woocommerce/index.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Plugin Name: Gateway AqayePardakht for Woocommerce
+  Version: 1.5
+  Description: این افزونه درگاه آقای پرداخت برای فروشگاه ساز ووکامرس میباشد.
+  Author: Aqaye Pardakht
+  Author URI: https://aqayepardakht.ir
+*/
+include_once( "class-wc-gateway-aqayepardakht.php" );
